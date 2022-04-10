b8cd1ceda03414305392df2dc9c5021210c16d4a
LiliPili <lilusenok@gmail.com>
2022-04-10 22:19:50 +0300

    Ch2

--- task_1/solution.js
+++ task_1/solution.js
@@ -9,19 +9,19 @@ function addInCartMessage(title, price) {
     // присвойте переменной message значение переменной productName
     message = productName;    
     // добавьте в конец переменной message пробел
-    message = message + " ";
+    message = message + ' ';
     // добавьте в конец переменной message текст 'за'
-    message = message + "за"; 
+    message = message + 'за'; 
     // добавьте в конец переменной message пробел
-    message = message + " ";
+    message = message + ' ';
     // добавьте в конец переменной message значение переменной productPrice
-    message = message + 'productPrice'
+    message = message + productPrice;
     // добавьте в конец переменной message пробел
-    message = message + " ";
+    message = message + ' ';
     // добавьте в конец переменной message текст 'теперь в корзине!'
-    message = message + "теперь в корзине";
+    message = message + 'теперь в корзине';
     // выведите переменную message в консоль
-    console.log(massage);
+    console.log (massage);
     // Конец решения задания №1.1.
 
     return message;
