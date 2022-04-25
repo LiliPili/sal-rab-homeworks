4cde1f2e1dd4fb96a7bf1ce24e11376c28fdc9bb
LiliPili <lilusenok@gmail.com>
2022-04-26 00:25:03 +0300

    chap7

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,8 +34,8 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+    let data = {client: {name, phone},order: {address, sum}, goods: [{title, count}]};
+    
     let countOfGoods = goods.length;
 
     for (let i = 0; i <= countOfGoods; i += 1) {
@@ -44,10 +44,9 @@ function sendRequest(name, phone, address, goods, sum) {
 
     data.order.address = address;
     data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
+ 
+    
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
