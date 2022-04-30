f25b77e55715febccce257e1bfedccac9b9dd973
LiliPili <lilusenok@gmail.com>
2022-04-30 15:10:56 +0300

    Chapter8

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,16 +34,20 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {client: {name, phone},order: {address, sum}, goods: [{title, count}]};
-    
-    let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    let data = {client: {}, goods: [], order: {}};
+    data.goods = [{title: title, count: count}]};
+    data.order.address = address {street, house, entrance, floor, flat};
+    data.client = ${name} + " " + ${phone};
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    let countOfGoods = goods.length;
+
+    for (let i = 0; i < countOfGoods; i += 1) {
+        data.goods.push({title:goods[i].title, count: goods[i].count});
+    }
+
+    
+    data.order.sum = sum;
  
     
     let jsonData = JSON.stringify({data});
